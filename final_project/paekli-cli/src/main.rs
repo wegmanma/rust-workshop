5907769c1d345cb65e277350ae5f294254d6ec02
Marcel Wegmann <wegr@zhaw.ch>
2024-04-12 11:59:23 +0200

    🍸 Foundation for CLI set with clap

--- final_project/paekli-cli/src/main.rs
+++ final_project/paekli-cli/src/main.rs
@@ -1,3 +1,11 @@
+use clap::Parser;
+
+/// send and receive pure distilled awesomeness 🍸
+#[derive(Parser)]
+#[clap(version)]
+struct Cli;
+
 fn main() {
+    let _args = Cli::parse();
     println!("Paekli LLC is currentli closed 😢");
 }
