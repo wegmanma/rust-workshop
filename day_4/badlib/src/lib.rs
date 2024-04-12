abcb4bbc9b70d3cc2ea93b23d14ffaed7c6e7428
Marcel Wegmann <wegr@zhaw.ch>
2024-04-12 11:15:14 +0200

    🛠️ fixing lints for github actions

--- day_4/badlib/src/lib.rs
+++ day_4/badlib/src/lib.rs
@@ -5,28 +5,32 @@
 //! We will incrementally build a CI pipeline to discover
 //! how CI can help us make sure our Rust code is squeaky clean.
 
+/// This function is not formatted correctly.
 pub fn badly_formatted() {
     println!("This code is badly formatted.");
     println!("Don't change it until CI fails because of it.");
 }
 
+/// This function is not idiomatic Rust.
 pub fn clippy_doesnt_like_this() {
     println!("This is ridiculous.");
 }
 
-pub fn dangerous_cast(n: i64) -> i32 {
+/// This function is not safe.
+pub fn dangerous_cast(n: i64) -> Result<i32, std::num::TryFromIntError>{
     // In my opinion, the `as` keyword should be used sparingly.
     // This conversion is lossy and may not do what you expect.
     // It is better to replace it with `.try_into()` and return
     // a `Reslut<i64, _>` instead.
     //
     // But as always, let's get CI to complain about it before we fix it.
-    n as i32
-}
+    n.try_into()
 
+}
+/// This function is not efficient.
 pub fn add(left: usize, right: usize) -> usize {
     // I don't know how to add two numbers...
-    left - right
+    left + right
 }
 
 #[cfg(test)]
