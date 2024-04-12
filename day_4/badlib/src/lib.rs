55bb581f50a4198a59ca21ddfe52f3326faf67a6
Marcel Wegmann <wegr@zhaw.ch>
2024-04-12 11:16:26 +0200

    🤨 fixed formatting...

--- day_4/badlib/src/lib.rs
+++ day_4/badlib/src/lib.rs
@@ -17,7 +17,7 @@ pub fn clippy_doesnt_like_this() {
 }
 
 /// This function is not safe.
-pub fn dangerous_cast(n: i64) -> Result<i32, std::num::TryFromIntError>{
+pub fn dangerous_cast(n: i64) -> Result<i32, std::num::TryFromIntError> {
     // In my opinion, the `as` keyword should be used sparingly.
     // This conversion is lossy and may not do what you expect.
     // It is better to replace it with `.try_into()` and return
@@ -25,7 +25,6 @@ pub fn dangerous_cast(n: i64) -> Result<i32, std::num::TryFromIntError>{
     //
     // But as always, let's get CI to complain about it before we fix it.
     n.try_into()
-
 }
 /// This function is not efficient.
 pub fn add(left: usize, right: usize) -> usize {
